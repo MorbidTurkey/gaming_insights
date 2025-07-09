c608188880a87f3a6ae4e5b7ed16811f22abe0b8
MorbidTurkey <coolhand93luke@yahoo.com>
2025-07-09 20:18:42 +0200

    server update

--- dash_app.py
+++ dash_app.py
@@ -43,4 +43,5 @@ for k, v in app.callback_map.items():
     print(f"{k}: {v['outputs']}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=True)
